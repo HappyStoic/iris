6a6fea5214708443a3c92cf4204b08810a231f00
Martin Řepa <martin.repa@cdn77.com>
2022-04-30 11:43:20 +0200

    version 3.0 of spread experiments

--- experiments/spread/spread.py
+++ experiments/spread/spread.py
@@ -1,4 +1,3 @@
-import time
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import List, Dict, Set, Tuple, Callable
@@ -15,9 +14,7 @@ class Peer:
     edges: Set[Tuple[int, int]] = field(default_factory=set)
     rel_book: Dict[int, float] = field(default_factory=dict)
     rel: float = -1.0
-
-    def is_good(self) -> bool:
-        return self.rel > GOOD_PEER_REL_THRESHOLD
+    is_benign: bool = True
 
 
 @dataclass(frozen=True)
@@ -26,44 +23,41 @@ class Network:
     start: int
     connected: bool
 
-    def assign_normal_reliabilities(self, avg_rel: float):
-        reliabilities = random.normal(loc=avg_rel, scale=10, size=len(self.peers))
-
-        for peer, rel in zip(self.peers, reliabilities):
-            peer.rel = rel
-
-        # this cannot be done in previous cycle because it needs rel to be set first
-        self.__assign_random_rel_views()
-
-    def assign_malicious_rate(self, rate: float):
+    def assign_reliabilities(self, malicious_ratio: float, malicious_mean_rel: float, benign_mean_rel: float,
+                             trust_accuracy: float):
         peers_total = len(self.peers)
-        malicious_total = int(peers_total*rate)
+        malicious_total = int(peers_total*malicious_ratio)
+        benign_total = peers_total - malicious_total
 
         peer_indices = list(range(peers_total))
         random.shuffle(peer_indices)
 
-        for i, peer_idx in enumerate(peer_indices):
-            self.peers[peer_idx].rel = 0.0 if i < malicious_total else 100.0
+        rels = random.normal(loc=[malicious_mean_rel]*malicious_total + [benign_mean_rel]*benign_total, scale=0.15)
+
+        for i, (peer_idx, rel) in enumerate(zip(peer_indices, rels)):
+            self.peers[peer_idx].is_benign = i >= malicious_total
+            self.peers[peer_idx].rel = round_rel(rel)
 
         # this cannot be done in previous cycle because it needs rel to be set first
-        self.__assign_random_rel_views()
+        self.__assign_random_rel_views(trust_accuracy)
 
-    def __assign_random_rel_views(self):
+    def __assign_random_rel_views(self, trust_accuracy: float):
         for peer in self.peers:
             rel_book = {}
             for (f, t) in peer.edges:
                 # make "t" the other peer
                 if t == peer.idx:
                     t = f
-                rel_book[t] = random_rel_view(self.peers[t].rel)
+                rel_book[t] = random_rel_view(self.peers[t].rel, std_deviation=trust_accuracy)
             peer.rel_book = rel_book
 
 
 @dataclass(frozen=True)
 class SpreadScenario:
-    num_peers: int
-    each_tick: int
-    total_ticks: int
+    spread_n_peers: int
+    spread_tick_interval: int
+    spread_total_ticks: int
+    max_scenario_ticks: int
     recipient_picker: Callable[[Peer, List[int]], int]
     description: str
 
@@ -92,23 +86,35 @@ class ScenarioResult:
 
 
 @dataclass(frozen=True)
+class PeersParameters:
+    malicious_ratio: float
+    malicious_mean_reliability: float
+    benign_mean_reliability: float
+    trust_accuracy: float
+
+
+@dataclass(frozen=True)
 class NetworkResult:
     network_idx: int
+    parameters: PeersParameters
     scenario_results: List[ScenarioResult]
 
 
-def random_rel_view(real_rel: float, std_deviation=10) -> float:
-    rel = random.normal(loc=real_rel, scale=std_deviation)
-    return 0 if rel < 0 else 100 if rel > 100 else rel
+def round_rel(rel: float) -> float:
+    return 0.0 if rel < 0.0 else 1.0 if rel > 1.0 else rel
 
 
-def generate_network(n_peers: int, network_degree: int, verbose=False) -> Network:
+def random_rel_view(real_rel: float, std_deviation=0.1) -> float:
+    return round_rel(random.normal(loc=real_rel, scale=std_deviation))
+
+
+def generate_network(n_peers: int, verbose=False) -> Network:
     # generate peers
     peers = [Peer(idx=idx) for idx in range(n_peers)]
 
     # generate edges
     indices = list(range(n_peers))
-    degrees = random.poisson(lam=network_degree, size=n_peers)
+    degrees = random.poisson(lam=7, size=n_peers)
     for from_peer, degree in zip(peers, degrees):
         # poisson distribution can give us 0, we want at least one connection
         if degree == 0:
@@ -207,7 +213,7 @@ def evaluate(network: Network, scenario: SpreadScenario) -> ScenarioResult:
     spread_ticks = {}
     good_peers_notified = 0
 
-    total_good_peers = len([None for p in network.peers if p.is_good()])
+    total_good_peers = len([None for p in network.peers if p.is_benign])
     quarter_good_peers = total_good_peers / 4
     half_good_peers = total_good_peers / 2
     three_quarter_good_peers = 3 * quarter_good_peers
@@ -218,14 +224,14 @@ def evaluate(network: Network, scenario: SpreadScenario) -> ScenarioResult:
     three_quarter_peers = 3 * quarter_peers
     perc95_peers = 95 * len(network.peers) / 100
 
-    for tick in range(MAX_TICKS):
+    for tick in range(scenario.max_scenario_ticks):
         # read messages from inbox
         for (from_p, spread_to) in spread_inbox:
             msgs_history[tick].append((from_p, spread_to))
             if spread_to in received_peers:
                 spam_msgs += 1
 
-                if network.peers[spread_to].is_good():
+                if network.peers[spread_to].is_benign:
                     spam_msgs_good_peers += 1
 
                 # we don't want to send msg to peers that told us that they already know about the msg
@@ -240,7 +246,7 @@ def evaluate(network: Network, scenario: SpreadScenario) -> ScenarioResult:
             received_peers.add(spread_to)
             peers_ticks_offsets[spread_to] = tick
 
-            if network.peers[spread_to].is_good():
+            if network.peers[spread_to].is_benign:
                 good_peers_notified += 1
                 if not spread25_good_peers and good_peers_notified > quarter_good_peers:
                     spread25_good_peers = tick
@@ -278,16 +284,16 @@ def evaluate(network: Network, scenario: SpreadScenario) -> ScenarioResult:
         # send messages
         for p in received_peers:
             peers_tick = tick - peers_ticks_offsets[p]
-            if peers_tick > scenario.total_ticks:
+            if peers_tick > scenario.spread_total_ticks:
                 # timeout of ticks for this peer
                 continue
 
-            if peers_tick % scenario.each_tick != 0:
+            if peers_tick % scenario.spread_tick_interval != 0:
                 # this peer is not sending msgs in this tick
                 continue
 
             # throw dices for mocking reliability
-            dices = random.rand(min(scenario.num_peers, len(peers_possible_recipients[p]))) * 100
+            dices = random.rand(min(scenario.spread_n_peers, len(peers_possible_recipients[p])))
             for roll in dices:
                 if not peers_possible_recipients[p]:
                     break
@@ -324,10 +330,6 @@ def evaluate(network: Network, scenario: SpreadScenario) -> ScenarioResult:
     return res
 
 
-GOOD_PEER_REL_THRESHOLD = 75
-MAX_TICKS = 10000
-
-
 def reliability_first_picker(sender: Peer, recipients: List[int]) -> int:
     return max(recipients, key=lambda idx: sender.rel_book[idx])
 
@@ -352,61 +354,67 @@ def random_picker(_: Peer, recipients: List[int]) -> int:
     return random.choice(recipients)
 
 
-def run(testing_scenarios: List[SpreadScenario], n_networks=10) -> (List[Network], Dict[str, List[NetworkResult]]):
-    networks_peers = random.randint(low=2, high=50, size=n_networks)
-    networks_degrees = random.randint(low=1, high=(networks_peers // 2) + 1, size=n_networks)
+def run(testing_scenarios: List[SpreadScenario], max_peers=50, n_networks=10) -> (List[Network], Dict[float, List[NetworkResult]]):
+    networks_peers = random.randint(low=2, high=max_peers, size=n_networks)
 
-    testing_normal_reliabilities = [20.0, 40.0, 60.0, 80.0]
-    testing_malicious_rates = [0.0, 0.25, 0.50, 0.75]
+    malicious_ratios = [0.0, 0.25, 0.5, 0.75]
+    malicious_mean_reliabilities = [0.0, 0.25, 0.5, 0.75]
+    benign_mean_reliabilities = [0.25, 0.5, 0.75, 1.0]
+    trust_view_accuracies = [0.0, 0.05, 0.15, 0.25]
 
     networks_cnt = 0
     networks = []
     results = defaultdict(list)
-    for i, (n_peers, network_degree) in enumerate(zip(networks_peers, networks_degrees)):
-        network = generate_network(n_peers, network_degree)
+    for i, n_peers in enumerate(networks_peers):
+        network = generate_network(n_peers)
 
         # skipping not connected networks
         if not network.connected:
             continue
 
-        for normal_rel in testing_normal_reliabilities:
-            name = f"normal distribution of reliability with mean {normal_rel}"
+        for malicious_ratio in malicious_ratios:
 
-            network.assign_normal_reliabilities(normal_rel)
+            m_idx = random.randint(low=0, high=len(malicious_mean_reliabilities))
+            b_idx = random.randint(low=m_idx, high=len(benign_mean_reliabilities))
+            malicious_rel = malicious_mean_reliabilities[m_idx]
+            benign_rel = benign_mean_reliabilities[b_idx]
+            trust_accuracy = random.choice(trust_view_accuracies)
+
+            parameters = PeersParameters(malicious_ratio=malicious_ratio, benign_mean_reliability=benign_rel,
+                                         malicious_mean_reliability=malicious_rel, trust_accuracy=trust_accuracy)
+            # print("doing parameters ", parameters)
+
+            network.assign_reliabilities(malicious_ratio, malicious_rel, benign_rel, trust_accuracy)
             scenario_results = [evaluate(network, scenario) for scenario in testing_scenarios]
+            results[malicious_ratio].append(NetworkResult(network_idx=networks_cnt, parameters=parameters,
+                                                          scenario_results=scenario_results))
 
-            results[name].append(NetworkResult(network_idx=networks_cnt, scenario_results=scenario_results))
-
-        #     plot(network, scenario_results[-1])
+        #     plot(network, scenario_results[0])
         #     break
         # break
 
-        for malicious_rate in testing_malicious_rates:
-            name = f"{malicious_rate}% of malicious peers in the network"
-
-            network.assign_malicious_rate(malicious_rate)
-            scenario_results = [evaluate(network, scenario) for scenario in testing_scenarios]
-
-            results[name].append(NetworkResult(network_idx=networks_cnt, scenario_results=scenario_results))
-
         networks.append(network)
         networks_cnt += 1
-        if networks_cnt % 20 == 0:
+
+        if networks_cnt % 25 == 0:
             print(f"{networks_cnt}th network done and evaluated")
 
-    print(f"Finished, {networks_cnt}th network done and evaluated")
+    print(f"Done... {networks_cnt}th network done and evaluated")
     return networks, results
 
 
-def new_scenarios(num_peers: int, each_tick: int, total_ticks: int) -> List[SpreadScenario]:
+def new_scenarios(num_peers: int, each_tick: int, total_ticks: int, max_scenario_ticks=10000) -> List[SpreadScenario]:
     txt = f"spreading up to {num_peers} peers every {each_tick} tick for maximum {total_ticks} ticks with "
     return [
-        SpreadScenario(num_peers=num_peers, each_tick=each_tick, total_ticks=total_ticks,
-                       recipient_picker=reliability_first_picker, description=f"{txt} reliability first picker"),
-        SpreadScenario(num_peers=num_peers, each_tick=each_tick, total_ticks=total_ticks,
-                       recipient_picker=reliability_favor_picker, description=f"{txt} reliability favor picker"),
-        SpreadScenario(num_peers=num_peers, each_tick=each_tick, total_ticks=total_ticks,
-                       recipient_picker=random_picker, description=f"{txt} random picker")
+        SpreadScenario(spread_n_peers=num_peers, spread_tick_interval=each_tick, spread_total_ticks=total_ticks,
+                       max_scenario_ticks=max_scenario_ticks, recipient_picker=reliability_first_picker,
+                       description=f"{txt} reliability first picker"),
+        SpreadScenario(spread_n_peers=num_peers, spread_tick_interval=each_tick, spread_total_ticks=total_ticks,
+                       max_scenario_ticks=max_scenario_ticks, recipient_picker=reliability_favor_picker,
+                       description=f"{txt} reliability favor picker"),
+        SpreadScenario(spread_n_peers=num_peers, spread_tick_interval=each_tick, spread_total_ticks=total_ticks,
+                       max_scenario_ticks=max_scenario_ticks, recipient_picker=random_picker,
+                       description=f"{txt} random picker")
     ]
 
 
@@ -417,7 +425,7 @@ def test():
     testing_scenarios += new_scenarios(num_peers=99, each_tick=10, total_ticks=50)
     testing_scenarios += new_scenarios(num_peers=99, each_tick=1, total_ticks=0)
 
-    networks, rel_results = run(testing_scenarios, n_networks=10)
+    networks, rel_results = run(testing_scenarios, n_networks=5)
 
 
 if __name__ == "__main__":
